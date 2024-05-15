446e66367520d728a4fa0f7d5ff868af0b2ce95b
Your Name <you@example.com>
2024-05-15 23:07:21 +0800

    updata# Please enter the commit message for your changes. Lines starting

--- lakefile.lean
+++ lakefile.lean
@@ -11,4 +11,4 @@ lean_lib «Lean4HardExample» where
 lean_exe «lean4hardexample» where
   root := `Main
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4"@"v4.6.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4"@"v4.6.0-rc1"
